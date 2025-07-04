32774f5fe2a481cd6b118780e58a6ba0c117d731
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-07-04 10:21:29 +0800

    done

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
         //          ->hourly();
         if(config('app.device') == "HIK")
         {
-            $schedule->command('command:get_attendance_hk')->everyMinute();
+            $schedule->command('command:get_attendance_hk')->everyMinute()->withoutOverlapping();
             // $schedule->command('command:get_attendance')->everyMinute();
             // $schedule->command('command:send_attendance')->everyMinute();
         }
